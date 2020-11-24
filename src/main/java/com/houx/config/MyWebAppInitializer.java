9c0fb138fc4f5606c78ce555069b3248043382a9
houxiang <15291122393@163.com>
2020-11-25 00:40:45 +0800

    修改jsonâ

--- src/main/java/com/houx/config/MyWebAppInitializer.java
+++ src/main/java/com/houx/config/MyWebAppInitializer.java
@@ -1,31 +1,31 @@
-package com.houx.config;
-
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-/**
- * @Author: HouX
- * @Date: 2020/11/24
- * @Description:
- */
-public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-    //Spring IoC容器配置
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        //可以返回Spring 的Java配置文件数组
-        return new Class<?>[] {};
-    }
-
-    //DispatcherServlet的URI映射关系
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        //可以返回Spring 的Java配置文件数组
-        return new Class<?>[] {WebConfig.class};
-    }
-
-    //DispatcherServlet拦截内容
-    @Override
-    protected String[] getServletMappings() {
-        return new String[] {"*.do"};
-    }
-}
+// package com.houx.config;
+//
+// import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+// /**
+//  * @Author: HouX
+//  * @Date: 2020/11/24
+//  * @Description:
+//  */
+// public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//
+//     //Spring IoC容器配置
+//     @Override
+//     protected Class<?>[] getRootConfigClasses() {
+//         //可以返回Spring 的Java配置文件数组
+//         return new Class<?>[] {};
+//     }
+//
+//     //DispatcherServlet的URI映射关系
+//     @Override
+//     protected Class<?>[] getServletConfigClasses() {
+//         //可以返回Spring 的Java配置文件数组
+//         return new Class<?>[] {WebConfig.class};
+//     }
+//
+//     //DispatcherServlet拦截内容
+//     @Override
+//     protected String[] getServletMappings() {
+//         return new String[] {"*.do"};
+//     }
+// }
--- src/main/java/com/houx/config/WebConfig.java
+++ src/main/java/com/houx/config/WebConfig.java
@@ -1,35 +1,35 @@
-package com.houx.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-/**
- * @Author: HouX
- * @Date: 2020/11/24
- * @Description:
- */
-
-@Configuration
-//定义扫描的包，加载控制器
-@ComponentScan("com.*")
-//启用Spring Web MVC
-@EnableWebMvc
-public class WebConfig {
-
-    /**
-     * 创建视图解析器
-     * @return 视图解析器
-     */
-    @Bean(name = "viewResolver")
-    public ViewResolver ViewResolverinitViewResolver(){
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/jsp/");
-        viewResolver.setSuffix(",jsp");
-        return viewResolver;
-    }
-
-}
+// package com.houx.config;
+//
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.ViewResolver;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.view.InternalResourceViewResolver;
+//
+// /**
+//  * @Author: HouX
+//  * @Date: 2020/11/24
+//  * @Description:
+//  */
+//
+// @Configuration
+// //定义扫描的包，加载控制器
+// @ComponentScan("com.*")
+// //启用Spring Web MVC
+// @EnableWebMvc
+// public class WebConfig {
+//
+//     /**
+//      * 创建视图解析器
+//      * @return 视图解析器
+//      */
+//     @Bean(name = "viewResolver")
+//     public ViewResolver ViewResolverinitViewResolver(){
+//         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//         viewResolver.setPrefix("/WEB-INF/jsp/");
+//         viewResolver.setSuffix(",jsp");
+//         return viewResolver;
+//     }
+//
+// }
